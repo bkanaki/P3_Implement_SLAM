030278d22d4b67d4a4694d76b19480bbe99a513d
bhargav <bhargav@etacompute.com>
2018-08-13 12:27:10 -0700

    Updated files with final submission

--- robot_class.py
+++ robot_class.py
@@ -78,19 +78,25 @@ class robot:
             '''
            
         measurements = []
-        
-        ## TODO: iterate through all of the landmarks in a world
-        
-        ## TODO: For each landmark
-        ## 1. compute dx and dy, the distances between the robot and the landmark
-        ## 2. account for measurement noise by *adding* a noise component to dx and dy
-        ##    - The noise component should be a random value between [-1.0, 1.0)*measurement_noise
-        ##    - Feel free to use the function self.rand() to help calculate this noise component
-        ## 3. If either of the distances, dx or dy, fall outside of the internal var, measurement_range
-        ##    then we cannot record them; if they do fall in the range, then add them to the measurements list
-        ##    as list.append([index, dx, dy]), this format is important for data creation done later
-        
-        ## TODO: return the final, complete list of measurements
+        #import pdb; pdb.set_trace()
+        # iterate through all of the landmarks in a world
+        for i, landmark in enumerate(self.landmarks):
+            # For each landmark
+            # 1. compute dx and dy, the distances between the robot and the landmark
+            # 2. account for measurement noise by *adding* a noise component to dx and dy
+            #    - The noise component should be a random value between [-1.0, 1.0)*measurement_noise
+            #    - Feel free to use the function self.rand() to help calculate this noise component
+            #    - It may help to reference the `move` function for noise calculation
+            # 3. If either of the distances, dx or dy, fall outside of the internal var, measurement_range
+            #    then we cannot record them; if they do fall in the range, then add them to the measurements list
+            #    as list.append([index, dx, dy]), this format is important for data creation done later
+            dx = fabs(self.x - landmark[0]) + self.rand() * self.measurement_noise
+            dy = fabs(self.y - landmark[1]) + self.rand() * self.measurement_noise
+            
+            if dx < self.measurement_range and dy < self.measurement_range:
+                measurements.append([i, dx, dy])
+            
+        # return the final, complete list of measurements
         return measurements
 
 
